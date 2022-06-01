272445e67bbc0c1af8e7c4b810f47ee5e1ea404e
lee <1017020555@qq.com>
2022-06-01 22:14:09 +0800

    商户类型缓存

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.collection.ListUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +22,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryTypeList() {
+        List<String> shopType = stringRedisTemplate.opsForList().range("shop:type",0,-1);
+        if (shopType.isEmpty()){
+            List<ShopType> typeList = query().orderByAsc("sort").list();
+            stringRedisTemplate.opsForList().leftPushAll("shop:type",typeList.toString());
+        }
+
+        return Result.ok();
+    }
+
 
 }
